3e74fafa5f85ae925eb3f7da6233ae68e40bea14
Niks <42009476+cliquant@users.noreply.github.com>
2024-07-07 05:08:16 +0300

    added login and register route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
-export default function LoginForm() {
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Register from './components/auth/register.tsx';
+import Login from './components/auth/login.tsx';
+import { ThemeProvider } from "./components/theme-provider.tsx"
+
+export default function App() {
   return (
-    <>
-      <p>Hello World</p>
-    </>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Navigate to="/login" replace />} />
+              <Route path="/login" element={
+                  <Login />
+              } />
+              <Route path="/register" element={
+                  <Register />
+              } />
+          </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
